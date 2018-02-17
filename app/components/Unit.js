74ede327e0d17a48e5d11a38436a6e50ed6499e3
Vyacheslav Enis <venis@difane.com>
2018-02-17 22:22:32 +0200

    Refactored Unit

--- app/components/Unit.js
+++ app/components/Unit.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import {Button, Card} from 'semantic-ui-react'
+import {Card, Dropdown, Grid} from 'semantic-ui-react'
 import {EditableText} from '@blueprintjs/core'
 
 const moment = require('moment');
@@ -11,11 +11,15 @@ type Props = {
   created: Date,
   updated: Date,
   onTitleChanged: (title : string) => void,
-  body: React$Node,
+  onEditClicked: () => void,
+  onSaveClicked: () => void,
+  onDeleteClicked: () => void,
+  editBody: React$Node,
+  viewBody: React$Node,
+  isEditing: boolean,
 };
-type State = {};
 
-export default class Unit extends React.Component<Props, State> {
+export default class Unit extends React.Component<Props> {
 
   constructor(props: Props) {
     super(props);
@@ -27,14 +31,27 @@ export default class Unit extends React.Component<Props, State> {
       <Card fluid>
         <Card.Content>
           <Card.Header>
-            <EditableText defaultValue={this.props.title} onConfirm={this.props.onTitleChanged} placeholder="Click to Edit Title" />
-            <Button icon="delete" size="mini" floated="right" negative compact/>
+            <Grid columns='equal'>
+              <Grid.Row stretched>
+                <Grid.Column>
+                  <EditableText defaultValue={this.props.title} onConfirm={(value) => this.props.onTitleChanged(value)} placeholder="Click to Edit Title"/>
+                </Grid.Column>
+                <Grid.Column floated='right' width={1}>
+                  <Dropdown icon='ellipsis horizontal' compact floating>
+                    <Dropdown.Menu className='left'>
+                      {!this.props.isEditing && <Dropdown.Item icon="edit" text='Edit' onClick={()=>this.props.onEditClicked()}/>}
+                      {this.props.isEditing && <Dropdown.Item icon="save" text='Save' onClick={()=>this.props.onSaveClicked()}/>}
+                      <Dropdown.Item icon='delete' text='Delete' onClick={()=>this.props.onDeleteClicked()}/>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </Card.Header>
-          <Card.Meta>Updated {moment(this.props.updated).format("D, YYYY [at] HH:mm")}</Card.Meta>
+          <Card.Meta>Updated {moment(this.props.updated).format("D, YYYY [at] HH:mm:ss")}</Card.Meta>
         </Card.Content>
-        <Card.Content>
-          {this.props.body}
-        </Card.Content>
+        {this.props.isEditing && <Card.Content>{this.props.editBody}</Card.Content>}
+        {!this.props.isEditing && <Card.Content>{this.props.viewBody}</Card.Content>}
       </Card>
     );
   }
--- stories/unit.story.js
+++ stories/unit.story.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {host} from 'storybook-host';
-import { action } from '@storybook/addon-actions';
+import { action,decorateAction } from '@storybook/addon-actions';
 import 'semantic-ui-css/semantic.min.css';
 import Unit from "../app/components/Unit";
+
+var defaultProps = {
+  id: "123",
+  title: "Unit Title",
+  created: new Date(),
+  updated: new Date(),
+  onTitleChanged: action("Title changed"),
+  onEditClicked: action("Edit clicked"),
+  onSaveClicked: action("Save clicked"),
+  onDeleteClicked: action("Delete clicked"),
+  viewBody: <div>VIEW</div>,
+  editBody: <div>EDIT</div>,
+  isEditing: false,
+};
 
 storiesOf('Unit', module)
   .addDecorator(host({
@@ -12,14 +26,18 @@ storiesOf('Unit', module)
     height: 400,
     width: "100%",
     background: false,
+
   }))
-  .add('Simple unit', () => (
-    <Unit id="123" title="Some Unit Title" created={new Date()} updated={new Date()} onTitleChanged={action("Title changed")}/>
+  .add('View mode', () => (
+    <Unit {...defaultProps} />
   ))
-  .add('Unit without title', () => (
-    <Unit id="123" title="" created={new Date()} updated={new Date()} onTitleChanged={action("Title changed")}/>
+  .add('View mode no title', () => (
+    <Unit {...defaultProps} title=""/>
   ))
-  .add('Unit with very long title', () => (
-    <Unit id="123" onTitleChanged={action("Title changed")} title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." created={new Date()} updated={new Date()}/>
+  .add('View mode long title', () => (
+    <Unit {...defaultProps} title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+  ))
+  .add('Edit mode', () => (
+    <Unit {...defaultProps} isEditing={true}/>
   ))
 ;
