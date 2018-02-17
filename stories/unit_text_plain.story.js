9a505bb7126ae056acfcd259a108f28c6e9f3b3d
Vyacheslav Enis <venis@difane.com>
2018-02-18 00:37:19 +0200

    Refactored plain text

--- stories/unit_text_plain.story.js
+++ stories/unit_text_plain.story.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import { decorateAction } from '@storybook/addon-actions';
+import { action } from '@storybook/addon-actions';
 import {host} from 'storybook-host';
 import 'semantic-ui-css/semantic.min.css';
 import UnitTextPlain from "../app/components/UnitTextPlain";
@@ -11,7 +11,12 @@ var defaultProps = {
   title: "Unit Title",
   created: new Date(),
   updated: new Date(),
-  onTitleChanged: decorateAction([args => args]),
+  onTitleChanged: action("Title changed"),
+  onEditClicked: action("Edit clicked"),
+  onSaveClicked: action("Save clicked"),
+  onDeleteClicked: action("Delete clicked"),
+  isEditing: false,
+  data: 'Some data'
 };
 
 storiesOf('UnitTextPlain', module)
@@ -22,10 +27,16 @@ storiesOf('UnitTextPlain', module)
     width: "100%",
     background: false,
   }))
-  .add('Normal mode with small amount of text', () => (
+  .add('View mode with small amount of text', () => (
     <UnitTextPlain {...defaultProps} />
   ))
-  .add('Normal mode with a couple paragraphs', () => (
-    <UnitTextPlain {...defaultProps} />
+  .add('View mode with a couple paragraphs', () => (
+    <UnitTextPlain {...defaultProps} data='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
+  ))
+  .add('Edit mode with small amount of text', () => (
+    <UnitTextPlain {...defaultProps} isEditing={true}/>
+  ))
+  .add('Edit mode with a couple paragraphs', () => (
+    <UnitTextPlain {...defaultProps} isEditing={true} data='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
   ))
   ;
