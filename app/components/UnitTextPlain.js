06e610d9acce9ce56af59ef9f0eead6541c7530b
Vyacheslav Enis <venis@difane.com>
2018-02-18 01:25:24 +0200

    Refactored TextPlain to make edit and view similar

--- app/components/UnitTextPlain.js
+++ app/components/UnitTextPlain.js
@@ -2,23 +2,24 @@
 import React from 'react';
 import {Container, Form, TextArea} from 'semantic-ui-react'
 import Unit from "./Unit";
+import styles from "./UnitTextPlain.css"
 
 type Props = {
   id: string,
   title: string,
   created: Date,
   updated: Date,
-  onTitleChanged: (title : string) => void,
+  onTitleChanged: (title: string) => void,
   onEditClicked: () => void,
   onSaveClicked: () => void,
   onDeleteClicked: () => void,
   isEditing: boolean,
-  data:string,
+  data: string,
 };
 type State = {
   data: string,
 };
-export default class UnitTextPlain extends React.Component<Props,State> {
+export default class UnitTextPlain extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props);
@@ -31,16 +32,23 @@ export default class UnitTextPlain extends React.Component<Props,State> {
   };
 
   render() {
-    const editBody =
-      <Form>
-        <TextArea autoHeight placeholder='Add some text here...' style="width:100%"
-                  onChange={(e, d) => this.handleDataChange(e, d)} value={this.state.data}/>
-      </Form>;
-
-    const viewBody = <Container fluid>{this.state.data}</Container>;
-
-    return (
-      <Unit {...this.props} viewBody={viewBody} editBody={editBody}/>
-    )
+    const editBodyProps = {
+      autoHeight: true,
+      placeholder: ' Add some text here...',
+      style: {
+        padding: 0,
+        fontFamily: 'monospace',
+        fontSize: '14px',
+        lineHeight: '18px',
+        border: '0',
+      },
+      onChange: (e, d) => this.handleDataChange(e, d),
+      value: this.state.data,
+      rows: 1,
+    };
+    return (<Unit {...this.props}
+                  viewBody={<Container fluid className={styles.view_container}>{this.state.data}</Container>}
+                  editBody={<Form><TextArea {...editBodyProps} /></Form>}
+    />)
   };
 }
